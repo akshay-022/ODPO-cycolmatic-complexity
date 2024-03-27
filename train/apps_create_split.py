1e0930adb13420b4fd0e218808f87bf156f42af5
madhushri1997 <madhumitha.shridharan@gmail.com>
2024-03-27 13:11:22 -0400

    minor edits to apps_create_split.py to fix some directory issues in generate samples clode

--- train/apps_create_split.py
+++ train/apps_create_split.py
@@ -4,7 +4,8 @@ import pathlib
 
 def create_split(split="train", name="train"):
     paths = []
-    roots = sorted(os.listdir(split))
+    parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    roots = sorted(os.listdir(os.path.join(parent_dir,split)))
     for folder in roots:
         root_path = os.path.join(split, folder)
         paths.append(root_path)
@@ -17,7 +18,7 @@ def create_split(split="train", name="train"):
 
 # insert path to train and test
 # path should be relative to root directory or absolute paths
-paths_to_probs = ["../data/APPS/train", "../data/APPS/test"]
+paths_to_probs = ["data/APPS/train", "data/APPS/test"]
 names = ["train", "test"]
 
 all_paths = []
